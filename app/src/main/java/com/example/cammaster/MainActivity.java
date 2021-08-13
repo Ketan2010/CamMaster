ec8a36f9e5cbdf86c6f41e5271ba12bd2fbccd4c
ketan patil <k2p1020@gmail.com>
2021-08-14 00:04:14 +0530

    Onboarding screen

---
+++ app/src/main/java/com/example/cammaster/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cammaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    // display time of onboarding screen
+    private static int TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //hide title bar of onboarding screen
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // move from onborading activity to login
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
